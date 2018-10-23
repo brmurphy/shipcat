d76154d47cf8a05c1d2fc4260cd20b6de8051646
Eirik Albrigtsen <eirik.albrigtsen@babylonhealth.com>
2018-10-23 09:18:04 +0100

    Update documentation

---
+++ implementors/slack_hook_clux/trait.TryFrom.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["slack_hook_clux"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
